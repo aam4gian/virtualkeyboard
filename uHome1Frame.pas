fda28ab3c3452f3f4459fd855f36982ebfe66b19
aam4gian <62690731+aam4gian@users.noreply.github.com>
2020-04-12 20:41:54 +0700

    Add files via upload

--- uHome1Frame.pas
+++ uHome1Frame.pas
@@ -6,8 +6,8 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   FMX.Objects, FMX.Controls.Presentation, FMX.Edit, FMX.Layouts, ALFmxLayouts,
-  ALFmxObjects, ALFmxEdit, ALCommon, ALFmxStdCtrls, System.Messaging, DW.VirtualKeyboard.Helpers,
-  FMX.Toast;
+  ALFmxObjects, ALFmxEdit, ALCommon, ALFmxStdCtrls, System.Messaging,
+  DW.VirtualKeyboard.Helpers;
 
 type
   TFrameHome1 = class(TFrame)
@@ -36,20 +36,12 @@ type
     ALEdit21: TALEdit;
     ALRectangle1: TALRectangle;
     Text2: TText;
-    Rectangle1: TRectangle;
-    ALTrackBar1: TALTrackBar;
     Layout1: TLayout;
-    ALTrackBar2: TALTrackBar;
-    Layout2: TLayout;
-    FMXToast1: TFMXToast;
     procedure ALRectangle1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
     procedure ALRectangle1MouseLeave(Sender: TObject);
     procedure ALVertScrollBox1ViewportPositionChange(Sender: TObject;
       const OldViewportPosition, NewViewportPosition: TPointF);
-    procedure ALTrackBar1Tracking(Sender: TObject);
-    procedure ALTrackBar2Tracking(Sender: TObject);
-    procedure FrameResize(Sender: TObject);
   private
     { Private declarations }
     FY: Single;
@@ -63,6 +55,7 @@ type
     procedure ReleaseFrame;
     procedure SetYFocus(aY, aH: Single);
     procedure SetFVKBoundHeight(ABoundH: Integer);
+    procedure InitEditPos(aPortY: Single);
   end;
 
 implementation
@@ -80,253 +73,230 @@ begin
   ALRectangle1.shadow.enabled := False;
 end;
 
-procedure TFrameHome1.ALTrackBar1Tracking(Sender: TObject);
-begin
-  Layout1.Margins.Top := ALTrackBar1.Value;
-  AlVertScrollBox1.AniCalculations.ViewportPosition :=
-    TALPointD.Create(0,ALTrackBar1.Value);
-end;
-
-procedure TFrameHome1.ALTrackBar2Tracking(Sender: TObject);
-begin
-  Layout2.Margins.Top := ALTrackBar2.Value;
-  AlVertScrollBox1.AniCalculations.ViewportPosition :=
-    TALPointD.Create(0,-ALTrackBar2.Value);
-end;
-
 procedure TFrameHome1.ALVertScrollBox1ViewportPositionChange(Sender: TObject;
   const OldViewportPosition, NewViewportPosition: TPointF);
 begin
-  if ALEdit1.Position.Y < NewViewportPosition.Y then
+  InitEditPos(NewViewportPosition.Y);
+end;
+
+procedure TFrameHome1.InitEditPos(aPortY: Single);
+begin
+  if ALEdit1.Position.Y < aPortY then
     ALEdit1.RemoveNativeView
   else
   begin
-    if (ALEdit1.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit1.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit1.Height) then
       ALEdit1.RemoveNativeView
     else ALEdit1.AddNativeView;
   end;
 
-  if ALEdit2.Position.Y < NewViewportPosition.Y then
+  if ALEdit2.Position.Y < aPortY then
     ALEdit2.RemoveNativeView
   else
   begin
-    if (ALEdit2.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit2.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit2.Height) then
       ALEdit2.RemoveNativeView
     else ALEdit2.AddNativeView;
   end;
 
-  if ALEdit3.Position.Y < NewViewportPosition.Y then
+  if ALEdit3.Position.Y < aPortY then
     ALEdit3.RemoveNativeView
   else
   begin
-    if (ALEdit3.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit3.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit3.Height) then
       ALEdit3.RemoveNativeView
     else ALEdit3.AddNativeView;
   end;
 
-  if ALEdit4.Position.Y < NewViewportPosition.Y then
+  if ALEdit4.Position.Y < aPortY then
     ALEdit4.RemoveNativeView
   else
   begin
-    if (ALEdit4.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit4.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit4.Height) then
       ALEdit4.RemoveNativeView
     else ALEdit4.AddNativeView;
   end;
 
-  if ALEdit5.Position.Y < NewViewportPosition.Y then
+  if ALEdit5.Position.Y < aPortY then
     ALEdit5.RemoveNativeView
   else
   begin
-    if (ALEdit5.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit5.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit5.Height) then
       ALEdit5.RemoveNativeView
     else ALEdit5.AddNativeView;
   end;
 
-  if ALEdit6.Position.Y < NewViewportPosition.Y then
+  if ALEdit6.Position.Y < aPortY then
     ALEdit6.RemoveNativeView
   else
   begin
-    if (ALEdit6.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit6.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit6.Height) then
       ALEdit6.RemoveNativeView
     else ALEdit6.AddNativeView;
   end;
 
-  if ALEdit7.Position.Y < NewViewportPosition.Y then
+  if ALEdit7.Position.Y < aPortY then
     ALEdit7.RemoveNativeView
   else
   begin
-    if (ALEdit7.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit7.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit7.Height) then
       ALEdit7.RemoveNativeView
     else ALEdit7.AddNativeView;
   end;
 
-  if ALEdit8.Position.Y < NewViewportPosition.Y then
+  if ALEdit8.Position.Y < aPortY then
     ALEdit8.RemoveNativeView
   else
   begin
-    if (ALEdit8.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit8.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit8.Height) then
       ALEdit8.RemoveNativeView
     else ALEdit8.AddNativeView;
   end;
 
-  if ALEdit9.Position.Y < NewViewportPosition.Y then
+  if ALEdit9.Position.Y < aPortY then
     ALEdit9.RemoveNativeView
   else
   begin
-    if (ALEdit9.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit9.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit9.Height) then
       ALEdit9.RemoveNativeView
     else ALEdit9.AddNativeView;
   end;
 
-  if ALEdit10.Position.Y < NewViewportPosition.Y then
+  if ALEdit10.Position.Y < aPortY then
     ALEdit10.RemoveNativeView
   else
   begin
-    if (ALEdit10.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit10.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit10.Height) then
       ALEdit10.RemoveNativeView
     else ALEdit10.AddNativeView;
   end;
 
-  if ALEdit11.Position.Y < NewViewportPosition.Y then
+  if ALEdit11.Position.Y < aPortY then
     ALEdit11.RemoveNativeView
   else
   begin
-    if (ALEdit11.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit11.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit11.Height) then
       ALEdit11.RemoveNativeView
     else ALEdit11.AddNativeView;
   end;
 
-  if ALEdit12.Position.Y < NewViewportPosition.Y then
+  if ALEdit12.Position.Y < aPortY then
     ALEdit12.RemoveNativeView
   else
   begin
-    if (ALEdit12.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit12.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit12.Height) then
       ALEdit12.RemoveNativeView
     else ALEdit12.AddNativeView;
   end;
 
-  if ALEdit13.Position.Y < NewViewportPosition.Y then
+  if ALEdit13.Position.Y < aPortY then
     ALEdit13.RemoveNativeView
   else
   begin
-    if (ALEdit13.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit13.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit13.Height) then
       ALEdit13.RemoveNativeView
     else ALEdit13.AddNativeView;
   end;
 
-  if ALEdit14.Position.Y < NewViewportPosition.Y then
+  if ALEdit14.Position.Y < aPortY then
     ALEdit14.RemoveNativeView
   else
   begin
-    if (ALEdit14.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit14.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit14.Height) then
       ALEdit14.RemoveNativeView
     else ALEdit14.AddNativeView;
   end;
 
-  if ALEdit15.Position.Y < NewViewportPosition.Y then
+  if ALEdit15.Position.Y < aPortY then
     ALEdit15.RemoveNativeView
   else
   begin
-    if (ALEdit15.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit15.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit15.Height) then
       ALEdit15.RemoveNativeView
     else ALEdit15.AddNativeView;
   end;
 
-  if ALEdit16.Position.Y < NewViewportPosition.Y then
+  if ALEdit16.Position.Y < aPortY then
     ALEdit16.RemoveNativeView
   else
   begin
-    if (ALEdit16.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit16.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit16.Height) then
       ALEdit16.RemoveNativeView
     else ALEdit16.AddNativeView;
   end;
 
-  if ALEdit17.Position.Y < NewViewportPosition.Y then
+  if ALEdit17.Position.Y < aPortY then
     ALEdit17.RemoveNativeView
   else
   begin
-    if (ALEdit17.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit17.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit17.Height) then
       ALEdit17.RemoveNativeView
     else ALEdit17.AddNativeView;
   end;
 
-  if ALEdit18.Position.Y < NewViewportPosition.Y then
+  if ALEdit18.Position.Y < aPortY then
     ALEdit18.RemoveNativeView
   else
   begin
-    if (ALEdit18.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit18.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit18.Height) then
       ALEdit18.RemoveNativeView
     else ALEdit18.AddNativeView;
   end;
 
-  if ALEdit19.Position.Y < NewViewportPosition.Y then
+  if ALEdit19.Position.Y < aPortY then
     ALEdit19.RemoveNativeView
   else
   begin
-    if (ALEdit19.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit19.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit19.Height) then
       ALEdit19.RemoveNativeView
     else ALEdit19.AddNativeView;
   end;
 
-  if ALEdit20.Position.Y < NewViewportPosition.Y then
+  if ALEdit20.Position.Y < aPortY then
     ALEdit20.RemoveNativeView
   else
   begin
-    if (ALEdit20.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit20.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit20.Height) then
       ALEdit20.RemoveNativeView
     else ALEdit20.AddNativeView;
   end;
 
-  if ALEdit21.Position.Y < NewViewportPosition.Y then
+  if ALEdit21.Position.Y < aPortY then
     ALEdit21.RemoveNativeView
   else
   begin
-    if (ALEdit21.Position.Y - NewViewportPosition.Y) >
+    if (ALEdit21.Position.Y - aPortY) >
       (ALVertScrollBox1.Height - ALEdit21.Height) then
       ALEdit21.RemoveNativeView
     else ALEdit21.AddNativeView;
   end;
 end;
 
-procedure TFrameHome1.FrameResize(Sender: TObject);
-begin
-  {$IF Defined(ANDROID)}
-  //handle the action bar under lollipop
-  if FVKState then
-    AlVertScrollBox1.AniCalculations.ViewportPosition := AlVertScrollBox1.AniCalculations.MaxTarget.Point;
-  {$ENDIF}
-end;
-
 procedure TFrameHome1.InitFrame;
 begin
   ReleaseFrame;
   TMessageManager.DefaultManager.SubscribeToMessage(TVKStateChangeMessage, VKStateChangeMessageHandler);
-  AlVertScrollBox1.AniCalculations.ViewportPosition :=
-    TALPointD.Create(0,10);
-  AlVertScrollBox1.AniCalculations.ViewportPosition :=
-    TALPointD.Create(0,0);
-  ALTrackBar1.Max := AlVertScrollBox1.VScrollBar.Max;
-  ALTrackBar2.Max := AlVertScrollBox1.Height;
+  InitEditPos(AlVertScrollBox1.AniCalculations.ViewportPosition.Y);
 end;
 
 procedure TFrameHome1.ReleaseFrame;
@@ -347,19 +317,19 @@ end;
 
 procedure TFrameHome1.VKStateChangeMessageHandler(const Sender: TObject;
   const M: TMessage);
-var
-  rY: Single;
 begin
   if TVirtualKeyboard.IsVisible then
   begin
     FVKState := True;
-    AlVertScrollBox1.margins.Bottom := FBoundHVK;
+    Layout1.Margins.Top := AlVertScrollBox1.AniCalculations.ViewportPosition.Y;
+    AlVertScrollBox1.margins.Bottom := FBoundHVK - ALRectangle1.Height - ALRectangle1.Margins.Top;
     AlVertScrollBox1.VScrollBar.Value := AlVertScrollBox1.VScrollBar.Max;
     AlVertScrollBox1.AniCalculations.TouchTracking := [];
   end
   else
   begin
     FVKState := False;
+    Layout1.Margins.Top := 0;
     AlVertScrollBox1.margins.Bottom := 0;
     AlVertScrollBox1.AniCalculations.TouchTracking := [ttVertical];
   end;
